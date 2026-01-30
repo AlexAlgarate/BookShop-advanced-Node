4fdf5282d138946996d9d1b1744dbebabaa29476
Alex Algarate <alex.algarate@gmail.com>
2026-01-30 21:51:05 +0100

    feat: project structure

---
+++ jest.config.ts
@@ -0,0 +1,25 @@
+import type { Config } from 'jest';
+
+const config: Config = {
+  preset: 'ts-jest/presets/default-esm',
+  extensionsToTreatAsEsm: ['.ts'],
+  testEnvironment: 'node',
+  testMatch: ['**/*.test.ts'],
+  setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'],
+  moduleNameMapper: {
+    '^(\\.{1,2}/.*)\\.js$': '$1',
+    '^@domain/(.*)$': '<rootDir>/src/domain/$1',
+    '^@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '^@ui/(.*)$': '<rootDir>/src/ui/$1',
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        useESM: true,
+      },
+    ],
+  },
+};
+
+export default config;
