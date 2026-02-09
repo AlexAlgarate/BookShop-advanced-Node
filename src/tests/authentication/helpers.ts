6a3497a90634049a851f20823e2bbbf134660ca0
Alex Algarate <alex.algarate@gmail.com>
2026-02-09 13:13:50 +0100

    feat: add authentication helper in auth tests and setup test file

---
+++ src/tests/authentication/helpers.ts
@@ -0,0 +1,19 @@
+import { app } from '@ui/api';
+import request from 'supertest';
+import * as z from 'zod';
+
+const loginResponseSchema = z.object({
+  content: z.string(),
+});
+
+export const signupAndLogin = async (
+  email: string = 'example@example.com',
+  password: string = '1234'
+): Promise<string> => {
+  await request(app).post('/authentication/signup').send({ email, password });
+
+  const loginResponse = await request(app).post('/authentication/signin').send({ email, password });
+
+  const validateResponse = loginResponseSchema.parse(loginResponse.body);
+  return validateResponse.content;
+};
---
+++ src/tests/setup.ts
@@ -0,0 +1,26 @@
+import { environmentService } from '@infrastructure/services/environment-service';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongo: MongoMemoryServer;
+
+beforeAll(async () => {
+  environmentService.load();
+  mongo = await MongoMemoryServer.create();
+  const uri = mongo.getUri();
+
+  await mongoose.connect(uri);
+});
+
+afterEach(async () => {
+  const collections = (await mongoose.connection.db?.collections()) ?? [];
+
+  for (const collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongo.stop();
+});
