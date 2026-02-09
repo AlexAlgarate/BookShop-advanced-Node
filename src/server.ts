9e52327d6cec2d194050cf0e606b526f5a7fa406
Alex Algarate <alex.algarate@gmail.com>
2026-02-09 13:30:15 +0100

    feat: add load environment variables and sentry to server

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,36 @@
 import mongoose from 'mongoose';
-import { startHttpApi } from './ui/api';
+import * as Sentry from '@sentry/node';
 
+import { startHttpApi } from './ui/api';
+import { environmentService } from '@infrastructure/services/environment-service';
+
+const loadEnvironment = (): void => {
+  console.log('...loading environment');
+  environmentService.load();
+  console.log('environment loaded...');
+};
+
+const initializeSentry = (): void => {
+  const { SENTRY_DSN, ENVIRONMENT } = environmentService.get();
+
+  Sentry.init({
+    dsn: SENTRY_DSN,
+    environment: ENVIRONMENT,
+  });
+};
 const connectMongoDb = async (): Promise<void> => {
-  await mongoose.connect('mongodb://admin:admin123@localhost:27017/db?authSource=admin');
+  const { MONGO_USER, MONGO_PASSWORD, MONGO_HOST } = environmentService.get();
+
+  await mongoose.connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}/db?authSource=admin`
+  );
   console.log('Mongodb connected!');
 };
 
 const executeApp = async (): Promise<void> => {
   try {
+    loadEnvironment();
+    initializeSentry();
     await connectMongoDb();
     startHttpApi();
   } catch (error) {
