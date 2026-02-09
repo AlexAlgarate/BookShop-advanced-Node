a94df28f9eb62996564b09089c04a8db02f37bb3
Alex Algarate <alex.algarate@gmail.com>
2026-02-09 13:30:33 +0100

    feat: red tests for auth signup

---
+++ src/tests/authentication/signup.test.ts
@@ -0,0 +1,54 @@
+import request from 'supertest';
+import { faker } from '@faker-js/faker';
+
+import { app } from '@ui/api';
+
+describe('POST /authentication/signup', () => {
+  const AUTHENTICATION_URL = '/authentication/signup';
+
+  test('Email and password should be mandatory', async () => {
+    const response = await request(app).post(AUTHENTICATION_URL).send({});
+
+    expect(response.status).toBe(400);
+  });
+
+  test('Email should be unique', async () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+
+    const firstAttempResponse = await request(app).post(AUTHENTICATION_URL).send({
+      email,
+      password,
+    });
+    expect(firstAttempResponse.status).toBe(201);
+
+    const secondAttempResponse = await request(app).post(AUTHENTICATION_URL).send({
+      email,
+      password,
+    });
+    expect(secondAttempResponse.status).toBe(409);
+  });
+
+  test('Given a valid email and password, a new user is created', async () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+
+    const response = await request(app).post(AUTHENTICATION_URL).send({
+      email,
+      password,
+    });
+    expect(response.status).toBe(201);
+  });
+
+  test.skip('The password must be hashed when the user is created', async () => {
+    const email = faker.internet.email();
+    const password = faker.internet.password();
+
+    const response = await request(app).post(AUTHENTICATION_URL).send({
+      email,
+      password,
+    });
+
+    expect(response.body.content.password).not.toMatch(password);
+  });
+});
