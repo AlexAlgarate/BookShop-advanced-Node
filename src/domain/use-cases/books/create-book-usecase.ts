fe458c05234aa709cb531bf64e81c409d81552fb
Alex Algarate <alex.algarate@gmail.com>
2026-03-25 19:39:45 +0100

    feat: remove unneccessary validation in create book use case

--- src/domain/use-cases/books/create-book-usecase.ts
+++ src/domain/use-cases/books/create-book-usecase.ts
@@ -1,14 +1,11 @@
 import { Book, BookStatus } from '@domain/entities/Book';
 import { BookRepository } from '@domain/repositories/BookRepository';
 import { CreateBookQuery } from '@domain/types/book/CreateBookQuery';
-import { BusinessConflictError } from '@domain/types/errors';
 
 export class CreateBookUseCase {
   constructor(private readonly bookRepository: BookRepository) {}
 
   public async execute(query: CreateBookQuery): Promise<Book> {
-    this.validateBookCreationRules(query);
-
     const createdBook = await this.bookRepository.createOne({
       ...query,
       status: BookStatus.PUBLISHED,
@@ -16,15 +13,5 @@ export class CreateBookUseCase {
     });
 
     return createdBook;
-  }
-
-  private validateBookCreationRules(query: CreateBookQuery): void {
-    if (query.price < 1) {
-      throw new BusinessConflictError('Book price must be at least 1');
-    }
-
-    if (query.title.trim().length === 0) {
-      throw new BusinessConflictError('Book title cannot be empty');
-    }
   }
 }
